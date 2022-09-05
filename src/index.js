4821ff5d48959e6e71bda2585d86304d4d774485
TexHaPb <texhapb@bk.ru>
2022-09-05 19:21:03 +0300

    first task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Users from './components/users';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <Users />
   </React.StrictMode>
 );
 
