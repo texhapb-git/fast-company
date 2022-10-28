c03fd4c6f7335003ecfcc8fb1bd90c7ec9f875da
TexHaPb <texhapb@bk.ru>
2022-10-28 11:28:21 +0300

    login

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     plugins: ["react"],
     rules: {
-        indent: ["error", 4],
+        indent: ["warn", 4],
         semi: [2, "always"],
         "space-before-function-paren": ["error", { anonymous: "always", named: "never" }],
         quotes: ["error", "double", { allowTemplateLiterals: true }]
