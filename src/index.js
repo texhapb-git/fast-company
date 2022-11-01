aef9f92d606542354e575aa6d2efa5a68f1160d5
TexHaPb <texhapb@bk.ru>
2022-11-01 13:03:00 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import App from "./app";
+import App from "./app/App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/mainPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function MainPage() {
-    return (
-        <h1>Main Page</h1>
-    );
-};
-
-export default MainPage;
